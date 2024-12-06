5a43099b05fc28b7285b1ce0a7612687c32b02c7
Maria Estrella <98559654+mlestrella843@users.noreply.github.com>
2024-12-06 12:22:36 -0700

    Finishing

--- theme_switcher/src/ThemeContext.jsx
+++ theme_switcher/src/ThemeContext.jsx
@@ -16,3 +16,24 @@ export const ThemeProvider = ({ children }) => {
     )
 }
 export const useTheme = () => useContext(ThemeContext);
+
+
+
+// import { createContext, useContext, useState } from "react";
+
+// const ThemeContext = createContext(undefined);
+
+// export const ThemeProvider = ({ children }) => {
+//  const [theme, setTheme] = useState("light");
+
+//  return (
+//    <ThemeContext.Provider
+//      value={{
+//        theme,
+//        toggleTheme: () => setTheme(theme === "light" ? "dark" : "light"),
+//      }}
+//    >
+//      {children}
+//    </ThemeContext.Provider>
+//  );
+// };
